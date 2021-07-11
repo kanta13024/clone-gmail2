72c0beda4ad227194fc42432d5766576374cbe93
Kanta <kanta@KantanoMacBook-Pro.local>
2021-07-11 23:48:32 +0900

    redux-slicer congig

---
+++ src/components/EmailRow.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "../assets/css/EmailRow.css";
+import Checkbox from "@material-ui/core/Checkbox";
+import { IconButton } from "@material-ui/core";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
+import { useHistory } from "react-router";
+
+function EmailRow({ id, title, subject, description, time }) {
+  const history = useHistory();
+
+  return (
+    <div onClick={() => history.push("/mail")} className="emailRow">
+      <div className="emailRow__options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
+
+      <h4>{title}</h4>
+
+      <div className="emailRow__message">
+        <h4>
+          {subject}
+          {"　"}
+          <span className="emailRow__description">-{description}</span>
+        </h4>
+      </div>
+
+      <p className="emailRow__time">{time}</p>
+    </div>
+  );
+}
+
+export default EmailRow;
--- src/components/Mail.js
+++ src/components/Mail.js
@@ -1,10 +1,78 @@
 import React from "react";
 import "../assets/css/Mail.css";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import MoveToInboxIcon from "@material-ui/icons/MoveToInbox";
+import ErrorIcon from "@material-ui/icons/Error";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EmailIcon from "@material-ui/icons/Email";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import UnfoldMoreIcon from "@material-ui/icons/UnfoldMore";
+import PrintIcon from "@material-ui/icons/Print";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import { IconButton } from "@material-ui/core";
+import { useHistory } from "react-router";
 
 function Mail() {
+  const history = useHistory();
+
   return (
     <div className="mail">
-      <div>Mail</div>
+      <div className="mail__tools">
+        <div className="mail__toolsLeft">
+          <IconButton onClick={() => history.push("/")}>
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton>
+            <MoveToInboxIcon />
+          </IconButton>
+          <IconButton>
+            <ErrorIcon />
+          </IconButton>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+          <IconButton>
+            <EmailIcon />
+          </IconButton>
+          <IconButton>
+            <WatchLaterIcon />
+          </IconButton>
+          <IconButton>
+            <CheckCircleIcon />
+          </IconButton>
+          <IconButton>
+            <LabelImportantIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+
+        <div className="mail__toolsRight">
+          <IconButton>
+            <UnfoldMoreIcon />
+          </IconButton>
+          <IconButton>
+            <PrintIcon />
+          </IconButton>
+          <IconButton>
+            <ExitToAppIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="mail__body">
+        <div className="mail__bodyHeader">
+          <h2>Subject</h2>
+          <LabelImportantIcon className="mail__important" />
+          <p>Title</p>
+          <p className="mail__time">10pm</p>
+        </div>
+
+        <div className="mail__message">This is a message</div>
+      </div>
     </div>
   );
 }
